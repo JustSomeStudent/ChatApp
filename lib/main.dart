721c56a53602e6514845ceb2c29d990f60a68bc7
ducklover420 <justasvanc@gmail.com>
2022-03-22 22:06:43 +0200

    added .dart files

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mix_chat_app/screens/login_screen.dart';
 
 void main() {
   runApp ( const MyApp() );
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold (
-
-        appBar: AppBar(
-          backgroundColor: Colors.blueAccent,
-          centerTitle: true,
-          title: const Text ('MixChat v0.1'),
-        ),
-
-        backgroundColor: Colors.blueGrey,
-
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children:  [
-              const Text ('Prisijungimas',
-                style: TextStyle(fontSize: 40),
-              ),
-
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  icon: Icon(Icons.perm_identity),
-                  labelText: 'Vartotojo vardas',
-
-                ),
-              ),
-              TextFormField(
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  icon: Icon(Icons.perm_identity),
-                  labelText: 'Slaptažodis',
-
-                ),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(150, 40),
-                    shape: const StadiumBorder()
-                ),
-                  child: const Text("LOGIN"),
-                  onPressed: (){},
-              )
-            ],
-          ),
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("lib/img/kalnai.jpg"),
-              fit: BoxFit.cover,
-              opacity: 0.5,
-            ),
-          ),
-        ),
-      ),
+    return const MaterialApp(
+      home: SignIn(),
     );
 
   }
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold (
+
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent,
+          centerTitle: true,
+          title: const Text ('MixChat v0.1'),
+        ),
+
+        backgroundColor: Colors.blueGrey,
+
+        body: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children:  [
+              const Text ('Prisijungimas',
+                style: TextStyle(fontSize: 40),
+              ),
+
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.perm_identity),
+                  labelText: 'Vartotojo vardas',
+
+                ),
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  icon: Icon(Icons.perm_identity),
+                  labelText: 'Slaptažodis',
+
+                ),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(150, 40),
+                    shape: const StadiumBorder()
+                ),
+                child: const Text("LOGIN"),
+                onPressed: (){},
+              )
+            ],
+          ),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("lib/img/kalnai.jpg"),
+              fit: BoxFit.cover,
+              opacity: 0.5,
+            ),
+          ),
+        ),
+      ),
+    );
+
+  }
+}
