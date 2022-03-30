31de31d48cc02c128e73afb3b407d0eac82a20a4
Mantvydas1 <manmil3@ktu.lt>
2022-03-30 23:59:32 +0300

    Sukuriau paskyros langą ir baigiau langų surišimą(kad būtu galima naviguoti)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mix_chat_app/screens/account_screen.dart';
 import 'package:mix_chat_app/screens/forgot_password_screen.dart';
 import 'package:mix_chat_app/screens/login_screen.dart';
 import 'package:mix_chat_app/screens/registration_screen.dart';
