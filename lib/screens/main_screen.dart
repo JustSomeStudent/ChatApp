f5fe41115948a142ac6ad1104d443949903f6f85
ducklover420 <justasvanc@gmail.com>
2022-03-31 15:14:52 +0300

    sukurtas pagrindinis ekranas

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mix_chat_app/screens/conversation_screen.dart';
+import 'package:mix_chat_app/screens/login_screen.dart';
 
 class ChatRoom extends StatefulWidget {
   const ChatRoom({Key? key}) : super(key: key);
@@ -10,9 +12,59 @@ class ChatRoom extends StatefulWidget {
 class _ChatRoomState extends State<ChatRoom> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return MaterialApp(
+      home: Scaffold (
+          appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title: const Text ('Main Screen: Contacts'),
+        actions: [
+          GestureDetector(
+            onTap: (){
+              Navigator.pushReplacement(context, MaterialPageRoute(
+                  builder: (context) => SignIn()
+              ));
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Icon(Icons.exit_to_app)),
+          )
+        ],
       ),
-    );
+          floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.search),
+        onPressed: (){
+        },
+      ),
+          body: Container(
+
+            child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      minimumSize: const Size(150, 40),
+                      shape: const StadiumBorder()
+                  ),
+                  child: const Text('Send Message'),
+                  onPressed: (){
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Conversation()
+                        ));
+                    },
+                )
+            ),
+
+
+/*
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("lib/img/kalnai.jpg"),
+                fit: BoxFit.cover,
+                opacity: 0.5,
+              ),
+            ),
+
+ */
+
+          )
+      );
   }
 }
