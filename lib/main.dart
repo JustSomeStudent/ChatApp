03cc17330ac2a73aa90c88419b20306e8e83b007
ducklover420 <justasvanc@gmail.com>
2022-03-31 15:13:14 +0300

    add/removing packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mix_chat_app/screens/forgot_password_screen.dart';
 import 'package:mix_chat_app/screens/login_screen.dart';
-import 'package:mix_chat_app/screens/registration_screen.dart';
 
 
 void main() {
@@ -13,7 +11,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: SignIn(),
     );
