eb89e20793a6c6904d9cb38f0da42d2b0f75f4dc
ducklover420 <justasvanc@gmail.com>
2022-03-31 15:15:10 +0300

    sukurtas susirašinėjimo langas

---
+++ lib/screens/conversation_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Conversation extends StatefulWidget {
+  const Conversation({Key? key}) : super(key: key);
+
+  @override
+  State<Conversation> createState() => _ConversationState();
+}
+
+class _ConversationState extends State<Conversation> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title: const Text ('Conversation Screen'),
+
+      ),
+      body: Container(
+        //alignment: Alignment.bottomCenter,
+        child: Stack(
+          children: [
+
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                color: Color(0xff0000ff),
+                padding: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                          style: TextStyle(color: Colors.white),
+                          decoration: InputDecoration(
+                            hintText: "Send a message...",
+                            hintStyle: TextStyle(
+                              color: Colors.white54
+                            ),
+                            border: InputBorder.none
+                          )
+                        )),
+                    Container(
+                      height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              const Color(0xff0000ff),
+                              const Color(0xfffff00f)
+                            ]
+                          ),
+                            borderRadius: BorderRadius.circular(40)
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Icon(Icons.send)),
+                  ],
+                ),
+              ),
+            )
+          ]
+        )
+      )
+    );
+  }
+}
