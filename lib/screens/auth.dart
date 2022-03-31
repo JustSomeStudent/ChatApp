309da754bed5fbb12a0fe576603f01a41e9daa64
ducklover420 <justasvanc@gmail.com>
2022-03-31 12:30:18 +0300

    testing out Firebase

---
+++ lib/screens/auth.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthMethods{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  getCurrentUser(){
+    return _auth.currentUser;
+  }
+  /*
+  Future signInWithEmailAndPassword(String email, String password){
+    try {
+      AuthResult result = await _auth.
+    }catch(e){
+
+    }
+  }
+
+   */
+}
